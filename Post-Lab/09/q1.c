638f3bd87a51257d30faa2b22bb5a1849e41656f
Amna <142903458+amnaa26@users.noreply.github.com>
2025-05-05 06:09:25 +0500

    Create q1.c

---
+++ Post-Lab/09/q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+// Signal handler function
+void sigint_handler(int signum) {
+  fprintf(stdout, "Caught SIGINT signal (%d)\n", signum);
+  raise(SIGKILL);
+}
+
+void my_handler(int signum) {
+  fprintf(stdout, "Caught SIGUSR1 signal (%d)\n", signum);
+  raise(SIGKILL);
+}
+
+int main() {
+  // Registering signal handler for SIGINT
+  if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+    printf("Error setting up signal handler for SIGINT\n");
+    return 1;
+  }
+
+  if (signal(SIGUSR1, my_handler) == SIG_ERR) {
+    printf("Error setting up signal handler for SIGUSR1\n");
+    return 1;
+  }
+
+  printf("Press Ctrl+C to send a SIGINT signal\n");
+
+  kill(getpid(), SIGUSR1);
+  return 0;
+}
